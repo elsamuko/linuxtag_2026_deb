113788a3704d3167f1ead51fee0b521eff3ea78a
Samuel Albrecht <elsamuko@gmail.com>
2026-09-06 12:08:01 +0200

    add: build_all

---
+++ README.md
@@ -0,0 +1,19 @@
+# Debian Packaging Workshop
+
+## simple
+Simple app build with dpkg-deb
+
+## service
+Simple service app build with dpkg-deb and postinst/prerm
+
+## dh_simple
+Simple app build with debhelper
+
+## dh_service
+Simple service app build with debhelper
+
+## dh_python
+Simple app build with debhelper and python dependencies
+
+## dh_python_venv
+Simple app build with debhelper and python venv
---
+++ build_all.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# keep sudo available
+sudo -v
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" 2>/dev/null || exit
+done &
+SUDO_LOOP_PID=$!
+trap 'kill "$SUDO_LOOP_PID" 2>/dev/null' EXIT INT TERM
+
+FOLDERS=(
+    simple
+    service
+    dh_simple
+    dh_service
+    dh_python
+    dh_python_venv
+)
+
+for FOLDER in "${FOLDERS[@]}"; do
+    echo "Building $FOLDER"
+    (
+        cd "$FOLDER" || exit
+        ./make_deb.sh
+        ./test_deb.sh
+    )
+done
